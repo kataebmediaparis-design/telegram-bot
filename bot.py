1c8043f4b91bf1dbc576988a0110227deae12ddd
kataebmediaparis-design <kataeb.media.paris@gmail.com>
2025-08-02 17:00:44 +0200

    bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
+import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-import os
 
 TOKEN = os.environ.get("TELEGRAM_TOKEN")
 
@@ -11,3 +11,4 @@ if __name__ == '__main__':
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.run_polling()
+
